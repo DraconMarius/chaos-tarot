e11d009b7a77628b9e8ef598b36815cb9d9de9c6
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2023-02-03 20:57:53 -0800

    set up react MERN structure

---
+++ client/src/App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import ChatContainer from './pages/ChatContainer';
+import Card from './pages/Swipe';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import ChatSkele from './pages/chatSkeleton';
+import ProfileContainer from './pages/ProfileContainer';
+import NavigateTabs from './components/NavigationTabs';
+import Footer from './components/Footer';
+import Match from './pages/Match';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+  // uri: "http://localhost:3001/graphql",
+});
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  // We then use the setContext() method to retrieve an existing token from local storage and attach the JWT token to every request sent from the client.
+  // The back end will then use this information to verify the request:
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  //cache so we can store data in the browser
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  // const isLoggedIn = localStorage.getItem('id_token');
+
+  return (
+    //applying the ApolloProvider component to the App component, we can then use the useQuery() Hook in any component to make a GraphQL query.
+    <ApolloProvider client={client}>
+      {/* //router component to handle the navigation between pages */}
+      <Router>
+        <div>
+          {/* <PageContainer> */}
+          <NavigateTabs />
+          <Routes>
+            {/* <Route 
+          exact path="/" element={isLoggedIn ? (
+          ) : (<Navigate replace to="/login" />)} /> */}
+            <Route exact path="/skele" element={<ChatSkele />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/chat" element={<ChatContainer />} />
+            <Route exact path="/signup" element={<SignUp />} />
+            <Route exact path="/swipe" element={<Match />} />
+            <Route exact path="/profile" element={<ProfileContainer />} />
+          </Routes>
+          {/* </PageContainer> */}
+        </div>
+      </Router>
+      <Footer />
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  //using strict mode to help us catch bugs
+  <React.StrictMode>
+    {/* <BrowserRouter> */}
+    <App />
+    {/* </BrowserRouter> */}
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
