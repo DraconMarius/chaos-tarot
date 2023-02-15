361a051d0498de5f84ce11b13219055f9f4fb819
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2023-02-14 16:18:09 -0800

    working on tarotData.js

--- server/seeds/tarotData.js
+++ server/seeds/tarotData.js
@@ -3,224 +3,224 @@
 Rider Waite Smith based intepretation
 https://github.com/ekelen/tarot-api
 
-and personal intepretation
+
 */
 
 [
     {
         "name": "THE FOOL",
-        "upright": "",
-        "inverted": "",
+        "upright": "Innocence, New Beginnings, Wonder, Foolishness",
+        "inverted": "Naive, Being Taken Advantage Of, Recklessness",
         "suit": "Major"
     },
     {
         "name": "THE MAGICIAN",
-        "upright": "",
-        "inverted": "",
+        "upright": "Mastery, Willpower, Creation, Adaptation, Manifestations",
+        "inverted": "Deception, Cunning, Trickery, Craftiness",
         "suit": "Major"
     },
     {
         "name": "THE HIGH PRIESTESS",
-        "upright": "",
-        "inverted": "",
+        "upright": "Intuition, Divine Truth, Unconscious, Inner Voice",
+        "inverted": "Loss of Self, Repression, Secrets, Hidden Agendas",
         "suit": "Major"
     },
     {
         "name": "THE EMPRESS",
-        "upright": "",
-        "inverted": "",
+        "upright": "Beauty, Fertility, Nuturing, Luxury, Creativity",
+        "inverted": "Smothering, Excess, Selfishness, Strict",
         "suit": "Major"
     },
     {
         "name": "THE EMPEROR",
-        "upright": "",
-        "inverted": "",
+        "upright": "Structure, Ambition, Authority, Rotionality, Rules",
+        "inverted": "Tyrany, Anger, Chaos, Domination",
         "suit": "Major"
     },
     {
         "name": "THE HIEROPHANT",
-        "upright": "",
-        "inverted": "",
+        "upright": "Tradition, Legacy, Society, Organized Religions",
+        "inverted": "Servitude, Convention, Blind Faith",
         "suit": "Major"
     },
     {
         "name": "THE LOVERS",
-        "upright": "",
-        "inverted": "",
+        "upright": "Choices, Union, Love, Relationship",
+        "inverted": "Indecision, Conflict, Growing Apart",
         "suit": "Major"
     },
     {
         "name": "THE CHARIOT",
-        "upright": "",
-        "inverted": "",
+        "upright": "Self Control, Discipline, Triumph, Sucess, Movement",
+        "inverted": "Loss of Direction, Lack of Motivation, Loss of Control",
         "suit": "Major"
     },
     {
-        "name": "FORTITUDE",
-        "upright": "",
-        "inverted": "",
+        "name": "The STRENGTH",
+        "upright": "Courage, Inner Strength, Conviction, Compassion",
+        "inverted": "Doubt, Pride, Weakness, Cowardice, Blind to Reason",
         "suit": "Major"
     },
     {
         "name": "THE HERMIT",
-        "upright": "",
-        "inverted": "",
+        "upright": "Contemplation, Solitude, Insight, Awareness",
+        "inverted": "Isolation, Withdrawal, Loneliness, Rejection",
         "suit": "Major"
     },
     {
         "name": "WHEEL OF FORTUNE",
-        "upright": "",
-        "inverted": "",
+        "upright": "Fate, Karma, Destiny, Fortune, Cycles, Change",
+        "inverted": "Outside of Control, Clinging to Control, Change",
         "suit": "Major"
     },
     {
         "name": "JUSTICE",
-        "upright": "",
-        "inverted": "",
+        "upright": "Truth, Fairness, Law, Clarity, Cause and Effect",
+        "inverted": "Dishonesty, Unaccountability, Unfairness",
         "suit": "Major"
     },
     {
         "name": "THE HANGED MAN",
-        "upright": "",
-        "inverted": "",
+        "upright": "Sacrifice, Relinquish, Martyrdom, Suspended",
+        "inverted": "Stalling, Procrastination, Needless Sacrifice",
         "suit": "Major"
     },
     {
         "name": "DEATH",
-        "upright": "",
-        "inverted": "",
+        "upright": "Endings, New Beginnings, Change, Metamorphosis",
+        "inverted": "Holding On, Stagnation, Unchanging, Decay",
         "suit": "Major"
     },
     {
         "name": "TEMPERANCE",
-        "upright": "",
-        "inverted": "",
+        "upright": "Patience, Finding Meaning, Middle Ground",
+        "inverted": "Extremes, Excess, Lack of Harmony",
         "suit": "Major"
     },
     {
         "name": "THE DEVIL",
-        "upright": "",
-        "inverted": "",
+        "upright": "Materialism, Playfulness, Pleasure, Vices, Trapped",
+        "inverted": "Freedom, Release, Restoring Control",
         "suit": "Major"
     },
     {
         "name": "THE TOWER",
-        "upright": "",
-        "inverted": "",
+        "upright": "Upheaval, Disaster, Foundational Shifts, Big Changes",
+        "inverted": "Averting Crisis, Delaying the inevitable, Fear of Suffering",
         "suit": "Major"
     },
     {
         "name": "THE STAR",
-        "upright": "",
-        "inverted": "",
+        "upright": "Hope, Faith, Rejuvenation, Rebuilding, Healing, The Light in the Dark",
+        "inverted": "Discouragement, Faithlessness, Insecurity, Hopelessness",
         "suit": "Major"
     },
     {
         "name": "THE MOON",
-        "upright": "",
-        "inverted": "",
+        "upright": "Unconscious, Illusions, Intuition, Obfuscated",
+        "inverted": "Confusion, Fear, Misinterpretation, Misguided",
         "suit": "Major"
     },
     {
         "name": "THE SUN",
-        "upright": "",
-        "inverted": "",
+        "upright": "Joy, Sucess, Celebration, Abundance",
+        "inverted": "Negativity, Depression, Sadness, Grey",
         "suit": "Major"
     },
     {
-        "name": "THE LAST JUDGMENT",
-        "upright": "",
-        "inverted": "",
+        "name": "THE JUDGMENT",
+        "upright": "Reflection, Reckoning, Awakening",
+        "inverted": "Lack of Self Awareness, Doubt, Self Loathing",
         "suit": "Major"
     },
     {
         "name": "THE WORLD",
-        "upright": "",
-        "inverted": "",
+        "upright": "Fullfillment, Equilibrium, Completion",
+        "inverted": "Incompleted, Lack of Closure, Emptiness",
         "suit": "Major"
     },
     {
         "name": "Page of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Expliration, Excitement, Freedom",
+        "inverted": "Lack of Direction, Procrastination, Conflict",
         "suit": "Wands"
     },
     {
         "name": "Knight of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Action, Adventure, Fearlessness",
+        "inverted": "Anger, Impulsivity, Recklessness",
         "suit": "Wands"
     },
     {
         "name": "Queen of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Courage, Determination, Passion, Joy",
+        "inverted": "Selfishness, Insecurity, Jealousy",
         "suit": "Wands"
     },
     {
         "name": "King of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Big Picture, Leader, Overcoming Challenges",
+        "inverted": "Impulsive, Oberbearing, Impossible Expectations",
         "suit": "Wands"
     },
     {
         "name": "Ace of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Creation, Willpower, Inspiration, Desire",
+        "inverted": "Lack of Engery, Lack of Focus, Lack of Passion, Boredom",
         "suit": "Wands"
     },
     {
         "name": "Two of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Planning, Making Decisions, Leaving home",
+        "inverted": "Fear of Change, Playing Safe, Bad Planning",
         "suit": "Wands"
     },
     {
         "name": "Three of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Looking Ahead, Expansion, Rapid Growth",
+        "inverted": "Obstacles, Delay, Frustration",
         "suit": "Wands"
     },
     {
         "name": "Four of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Community, Home, Celebration",
+        "inverted": "Lack of Support, Transience, Home Conflict",
         "suit": "Wands"
     },
     {
         "name": "Five of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Competition, Conflict, Harsh Reality",
+        "inverted": "Avoiding Conflict, Respecting Differences",
         "suit": "Wands"
     },
     {
         "name": "Six of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Victory, Success, Public Regconition",
+        "inverted": "Excess Pride, Lack of Recognition, Punishment",
         "suit": "Wands"
     },
     {
         "name": "Seven of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Perseverance, Mounting A Defense, Maintaining Control",
+        "inverted": "Giving Up, Lack of confidence, Overwhelmed",
         "suit": "Wands"
     },
     {
         "name": "Eight of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Rapid Action, Movement, Quick Decisions",
+        "inverted": "Panic Waiting, Slowing Down, Procrastination",
         "suit": "Wands"
     },
     {
         "name": "Nine of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Resilience, Grit, Taking Last Stand",
+        "inverted": "Exhaustion, Fatigue, Unsure Motives",
         "suit": "Wands"
     },
     {
         "name": "Ten of Wands",
-        "upright": "",
-        "inverted": "",
+        "upright": "Accomplishment, Responsibility, Burden",
+        "inverted": "Burnt Out, Stressed, Unwilling to Delegate, Hoarding",
         "suit": "Wands"
     },
     {
